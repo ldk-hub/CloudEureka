df721c4e30c25ecc6e3da3534dfdf0ff69b2c863
Dongok Lee <orm6711@gmail.com>
2020-10-25 20:26:07 +0900

    유레카 서버 구성(서비스레지스트리)

---
+++ CloudEureka/src/test/java/com/msa/service/CloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
