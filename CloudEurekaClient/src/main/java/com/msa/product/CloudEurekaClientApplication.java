356c6a3adef2ee0f4c738e8508f8cbec47c01420
Dongok Lee <orm6711@gmail.com>
2020-10-25 20:36:49 +0900

    유레카 클라이언트(프로덕트api)

---
+++ CloudEurekaClient/src/main/java/com/msa/product/CloudEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.msa.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaClientApplication.class, args);
+	}
+
+}
