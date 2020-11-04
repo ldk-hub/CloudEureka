58e648e9039bdbd3c95fbf0b1b086f08f4f1d150
Dongok Lee <orm6711@gmail.com>
2020-11-04 22:40:58 +0900

    유레카 수정

--- CloudEureka/src/main/java/com/msa/service/CloudEurekaApplication.java
+++ CloudEureka/src/main/java/com/msa/service/CloudEurekaApplication.java
@@ -1,19 +1,19 @@
 package com.msa.service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-@EnableEurekaServer //또는 EnableDiscoveryClient를 사용해도됨.
+
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableEurekaServer 
 public class CloudEurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudEurekaApplication.class, args);
 	}
+	
+	
 
 }
